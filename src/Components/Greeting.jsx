b7731925192ea9f259e5380b604df279ddb69b49
dean <deanraguso@gmail.com>
2021-06-27 15:32:58 +1000

    Created: Time sensitive title that greets the user.

--- src/Components/Greeting.jsx
+++ src/Components/Greeting.jsx
@@ -1,7 +1,24 @@
+const generateGreeting = (hours) => {
+    if (hours < 6) {
+        return "Good morning early bird!"
+    } else if (hours < 12) {
+        return "Good morning."
+    } else if (hours < 17) {
+        return "Good afternoon."
+    } else if (hours < 20) {
+        return "Good evening."
+    } else {
+        return "Good night owl!"
+    }
+}
+
 const Greeting = () => {
+
+    let date = new Date();
+
     return (
         <h1 id="title">
-            Good Morning
+            {generateGreeting(date.getHours())}
         </h1>
     )
 }
